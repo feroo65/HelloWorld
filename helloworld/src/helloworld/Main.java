7538dee020784122abe1197782ee46653ee820b5
gie7kirfer <gie7kirfer@szt2-8-w10.vasvari.hu>
2019-09-07 12:04:50 +0200

    Mod 3

--- helloworld/src/helloworld/Main.java
+++ helloworld/src/helloworld/Main.java
@@ -9,6 +9,29 @@ public class Main {
 		System.out.println(args[0]);
 		System.out.println(args[1]);
 		System.out.println(args[2]);
+		
+		
+		int a, b, c,d ,e;
+		
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		c = Integer.parseInt(args[2]);
+		d = Integer.parseInt(args[3]);
+		e = Integer.parseInt(args[4]);
+		
+		
+		
+		System.out.println(a*b*c);
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+		System.out.println(a*b*c*d*e/5);
+		
+		
+		
 	}
 
 }
