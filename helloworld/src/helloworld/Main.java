315b753f1284d45b573a93256c70820bb822b208
gie7kirfer <gie7kirfer@szt2-8-w10.vasvari.hu>
2019-09-07 11:26:33 +0200

    Elso mod

--- helloworld/src/helloworld/Main.java
+++ helloworld/src/helloworld/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Vasvari!");
-
+		System.out.println("hossz: "+args.length);
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
 	}
 
 }
